b6616b0649bbe9af8ee288af347a73a32ac111f6
maseifert13 <122394696+maseifert13@users.noreply.github.com>
2023-02-17 15:20:28 -0500

    Add files via upload

---
+++ Project1Part2/README.md
@@ -0,0 +1,2 @@
+# Project1Part2
+ My website for Graphic Design - web design 2
---
+++ index.js
@@ -0,0 +1,30 @@
+
+const express = require("express");
+const app = express();
+const router = express.Router();
+const port = 5002
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`); 
+});
+
+app.get("/", function(req, res){
+	res.sendFile('index.html', {root: __dirname});  
+});
+
+// app.get("/about", function(req, res){
+	//res.sendFile('about.html', {root: __dirname})
+//});
+
+//app.get("/scripts/getWeather.js", function(req, res){
+	//res.sendFile('/scripts/getWeather.js', {root: __dirname});  
+//})
+
+app.use('/views', express.static('views'));
+
+app.use('/images', express.static('images'));
+
+app.use('/scripts', express.static('scripts'));
+
+
